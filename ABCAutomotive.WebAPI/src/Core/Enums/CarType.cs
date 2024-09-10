b6985ed665577cc29ba146e7ba378a446163b8c0
Mehul Bhavsar <m.bhavsar6@gmail.com>
2024-09-10 13:59:14 -0600

    remove rest http file

--- ABCAutomotive.WebAPI/src/Core/Enums/CarType.cs
+++ ABCAutomotive.WebAPI/src/Core/Enums/CarType.cs
@@ -2,7 +2,7 @@ namespace ABCAutomotive.WebAPI.Core.Enums;
 
 public enum CarType
 {
-    Regular,        // Represents a regular car
-    SUV,            // Represents an SUV
-    Convertible     // Represents a convertible
+    Regular,        
+    SUV,          
+    Convertible
 }
